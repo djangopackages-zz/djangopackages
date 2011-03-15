02e6ce9d9c71d4e0a3897f13c846ee7b014d9877
Jeff Schenck <jmschenck@gmail.com>
2011-03-14 17:22:16 -0700

    Adds management commands for PyPI updating

---
+++ apps/pypi/management/commands/pypi_updater.py
@@ -0,0 +1,16 @@
+from django.core.management.base import NoArgsCommand
+
+from pypi.handler import update_outdated_packages
+
+
+class Command(NoArgsCommand):
+    help = 'Updates all outdated packages on PyPI.'
+    
+    def handle(self, *args, **options):
+        print 'Beginning package update from PyPI at %s UTC.' % \
+            datetime.utcnow().strftime('%d %b %Y %H:%M:%S')
+
+        update_outdated_packages()
+
+        print 'Completed package update from PyPI at %s UTC.' % \
+            datetime.utcnow().strftime('%d %b %Y %H:%M:%S')
